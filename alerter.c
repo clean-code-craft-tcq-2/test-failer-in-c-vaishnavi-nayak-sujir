68c54de21c07574d30cc9714dc1c6781a690be49
vaishnavi-nayak-sujir <vaishnavinayak86@gmail.com>
2022-01-25 22:17:55 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -2,13 +2,22 @@
 #include <assert.h>
 
 int alertFailureCount = 0;
-
+int threshold=200;//let threshold be 200 deg celcius
 int networkAlertStub(float celcius) {
+
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+	if(celcius >= threshold)
+	{
+		return 500;
+	}
+	else
+	{
+		return 200;
+	}
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    return 200;
+    
 }
 
 void alertInCelcius(float farenheit) {
@@ -22,10 +31,19 @@ void alertInCelcius(float farenheit) {
         alertFailureCount += 0;
     }
 }
+int checkerror(float farenheit){
+	float celcius = (farenheit - 32) * 5 / 9;
+	if (celcius >=threshold)// if temp is greater than threshold alertcounter should increment
+	{
+		assert(alertFailureCount>0);
+	}
+}
 
 int main() {
     alertInCelcius(400.5);
+	checkerror(400.5);
     alertInCelcius(303.6);
+	checkerror(303.6);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
